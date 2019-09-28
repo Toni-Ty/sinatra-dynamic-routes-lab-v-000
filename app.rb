4ecef641022436b5519d000ab9d59d06b8928a83
Toni-Ty <antonique2479@yahoo.com>
2019-09-28 03:09:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,11 +29,11 @@ get '/say/:number/:phrase' do
 
 if params [:operation] = 'add'
     answer = (number1 + number2).to_s
-elsif operation = "subtract"
+elsif params [:operation] = "subtract"
   answer = (number1 - number2).to_s
-elsif operation = "multiply"
+elsif params [:operation] = "multiply"
   answer = (number1 * number2).to_s
-elsif operation = "divide"
+elsif params [:operation] = "divide"
   answer = (number1 / number2).to_s
   end
 end
