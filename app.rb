04f9a39e7e87bd61503a3da1840ac8df2afbd355
Toni-Ty <antonique2479@yahoo.com>
2019-09-28 03:06:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,6 +24,22 @@ get '/say/:number/:phrase' do
   end
 
   get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+
+if operation = "add"
+    answer = (number1 + number2).to_s
+elsif operation = "subtract"
+  answer = (number1 - number2).to_s
+elsif operation = "multiply"
+  answer = (number1 * number2).to_s
+elsif operation = "divide"
+  answer = (number1 / number2).to_s
+  end
+end
+end
+
+    
 
 
 
